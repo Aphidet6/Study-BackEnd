06a1d684692c3909d4f1a1f4eadabf187740d5f1
Aphidet Jamphakhun <earthhh001@gmail.com>
2025-09-16 19:02:22 +0700

    set up video

---
+++ Day-2/ex1.js
@@ -0,0 +1,9 @@
+let age = 17;
+
+// if (age < 18) {
+//     console.log("ห้ามเข้า");
+// } else {
+//     console.log("เข้าได้");
+// }
+
+age < 18 ? console.log("ห้ามเข้า") : console.log("เข้าได้");
