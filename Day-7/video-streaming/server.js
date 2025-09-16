886b66777e373cf0c5e75b16e42885ed182b193f
Aphidet Jamphakhun <earthhh001@gmail.com>
2025-09-16 22:53:32 +0700

    End Day 7

---
+++ Day-7/video-streaming/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const videoPath = 'video.mp4';
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/video', (req, res) => {
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
+    const videoSize = fs.statSync(videoPath).size;
+    const CHUNK_SIZE = 10 ** 6; // 1MB
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE - 1, videoSize - 1);
+    const contentLength = end - start + 1;
+
+    const headers = {
+        "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+    videoStream.pipe(res);
+});
+
+app.listen(3000, () => {
+    console.log(`Express server listening on port 3000`);
+});
