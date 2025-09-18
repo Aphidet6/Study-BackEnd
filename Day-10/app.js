a33f3ae9f2ebc3e05e72df431a5b9f6678f727ef
Earth <earthhh001@gmail.com>
2025-09-18 17:26:25 +0700

    set up half day 10

--- Day-10/app.js
+++ Day-10/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const db = require('./db');
 
-const users = [];
 
 const app = express();
 
@@ -16,16 +17,37 @@ app.post('/register', async (req, res) => {
     const { username, password } = req.body;
 
     //เช็คว่ามี username นี้ในระบบหรือไม่
-    const existingUser = users.find((user) => user.username === username);
-    if (existingUser) {
-            return res.status(400).json({ message: 'Username already exists' });
-    }
     try {
-        users.push({ username, password });
+        const [existingUser] = await db.execute('SELECT * FROM users WHERE username = ?', [username]);
+        if (existingUser.length > 0) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+        //เข้ารหัส password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        await db.execute('INSERT INTO users (username, hashedpassword) VALUES (?, ?)', [username, hashedPassword]);
         res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const [row] = await db.execute('SELECT * FROM users WHERE username = ?', [username]);
+        if (row.length === 0) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        const user = row[0];
+        //ตรวจสอบ password
+        const isPasswordValid = await bcrypt.compare(password, user.hashedpassword);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid password' });
+        }
+        res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }   
 });
 
 app.listen(3000, () => {
