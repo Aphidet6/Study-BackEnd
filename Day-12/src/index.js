92503903258595c11b325e52af65299e1e38a303
Earth <earthhh001@gmail.com>
2025-09-21 11:43:18 +0700

    d

--- Day-12/src/index.js
+++ Day-12/src/index.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth");
 const protectedRoutes = require("./routes/protected");
+const productRoutes = require("./routes/products");
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(cors());
 
 app.use("/auth", authRoutes);
 app.use("/protected", protectedRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(3000, () => {
   console.log("App listening on port 3000!");
