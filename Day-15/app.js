a904646a31662243f09abf728dd109ced723364f
Earth <earthhh001@gmail.com>
2025-09-25 16:09:06 +0700

    Fix MongoClient import and update connection URI in app.js

--- Day-15/app.js
+++ Day-15/app.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const { mongoClient } = require('mongodb');
+const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017';
+const uri = 'mongodb://127.0.0.1:27017';
 
 const app = express();
 app.use(bodyParser.json());
 
+
 app.post('/register', async (req, res) => {
-    const client = new mongoClient(uri);
+    const client = new MongoClient(uri);
+
+    try{
+        await client.connect();
+        const database = client.db('test');
+        const collection = database.collection('users');
+        const result = await collection.insertOne(req.body);
+        res.send(result);
+    } finally {
+        await client.close();
+    }
 })
+app.post('/login', async (req, res) => {
+    try{
+        await client.connect();
+        const database = client.db('test');
+        const collection = database.collection('users');
+        const result = collection.findOne(req.body);
+        res.send(result);
+    } finally {
+        await client.close();
+    }
+})
+
+app.listen(3000, () => {
+    console.log('http://localhost:3000');
+});
